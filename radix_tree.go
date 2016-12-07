280d44c5bf40c002cdb9ba5bba466dfd03640457
Aaron Windsor <aaron.windsor@gmail.com>
2016-12-07 07:16:50 -0500

    toasty

--- radix_tree.go
+++ radix_tree.go
@@ -1,11 +1,12 @@
 package radix_tree
 
 import (
+	"errors"
 	"fmt"
 )
 
 type RadixTree struct {
-	root *node
+	root node
 }
 
 type node interface {
@@ -13,8 +14,8 @@ type node interface {
 }
 
 type inode struct {
-	lc       *node
-	rc       *node
+	lc       node
+	rc       node
 	critbyte uint32
 	critmask uint8
 }
@@ -28,19 +29,83 @@ func (n tnode) isTerminal() bool { return true }
 func (n inode) isTerminal() bool { return false }
 
 func (t RadixTree) Get(key string) (val string, err error) {
-	return "", nil
+	n := t.root
+	var i uint32
+	for {
+		switch x := n.(type) {
+		case inode:
+			i = x.critbyte
+			if key[i]&x.critmask == 0 {
+				n = x.lc
+			} else {
+				n = x.rc
+			}
+		case tnode:
+			if len(key) != len(x.key) {
+				return "", errors.New("Not found")
+			}
+			for j := int(i); j < len(key); j++ {
+				if x.key[j] != key[j] {
+					return "", errors.New("Not found")
+				}
+			}
+			return x.val, nil
+		}
+	}
 }
 
 func (t *RadixTree) Set(key string, val string) {
-
+	n := &t.root
+	var i uint32
+	for {
+		if *n == nil {
+			*n = &tnode{key: key, val: val}
+			return
+		} else {
+			switch x := (*n).(type) {
+			case inode:
+				i = x.critbyte
+				if key[i]&x.critmask == 0 {
+					n = &x.lc
+				} else {
+					n = &x.rc
+				}
+			case tnode:
+				// TODO: can maybe speed this comp up by starting from i
+				equal := true
+				j := i
+				// TODO: figure out how to treat case where one string is
+				// a prefix of another. Always branch right on longer string?
+				minLen := len(key)
+				if len(x.key) > minLen {
+					minLen = len(x.key)
+				}
+				for ; equal && j < minLen; j++ {
+					if x.key[j] != key[j] {
+						equal = false
+					}
+				}
+				if equal {
+					x.val = val
+					return
+				} else {
+					// TODO: figure out differing bit, set vals in inode
+					// accordingly
+					in := inode{lc: nil, rc: nil, critbyte: 0, critmask: 0}
+					*n = &in
+					n = &in.lc
+				}
+			}
+		}
+	}
 }
 
 func (t RadixTree) DumpContents() {
 	dumpContents(t.root, 0)
 }
 
-func dumpContents(n *node, indent int) {
-	switch x := (*n).(type) {
+func dumpContents(n node, indent int) {
+	switch x := n.(type) {
 	case inode:
 		dumpContents(x.lc, indent+2)
 		dumpContents(x.rc, indent+2)
--- radix_tree_test.go
+++ radix_tree_test.go
@@ -1,9 +1,13 @@
 package radix_tree
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestRadixTree(t *testing.T) {
-
+	const nihongo = "日本語"
+	for i := 0; i < len(nihongo); i++ {
+		fmt.Printf("%v: %v\n", i, nihongo[i])
+	}
 }
