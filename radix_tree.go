7728f1e78552af6e3613156eed98a39225777b6a
Aaron Windsor <aaron.windsor@gmail.com>
2016-12-09 07:18:40 -0500

    toasty

--- radix_tree.go
+++ radix_tree.go
@@ -16,7 +16,7 @@ type node interface {
 type inode struct {
 	lc       node
 	rc       node
-	critbyte uint32
+	critbyte int
 	critmask uint8
 }
 
@@ -30,17 +30,23 @@ func (n inode) isTerminal() bool { return false }
 
 func (t RadixTree) Get(key string) (val string, err error) {
 	n := t.root
-	var i uint32
+	var i int
+	if n == nil {
+		return "", errors.New("Not found")
+	}
 	for {
+		fmt.Printf("loop: %T", n)
 		switch x := n.(type) {
-		case inode:
+		case *inode:
+			fmt.Printf("Going down inode %v\n", x)
 			i = x.critbyte
 			if key[i]&x.critmask == 0 {
 				n = x.lc
 			} else {
 				n = x.rc
 			}
-		case tnode:
+		case *tnode:
+			fmt.Printf("Going down tnode %v\n", x)
 			if len(key) != len(x.key) {
 				return "", errors.New("Not found")
 			}
@@ -54,24 +60,36 @@ func (t RadixTree) Get(key string) (val string, err error) {
 	}
 }
 
+func msb_mask(b byte) byte {
+	b |= b >> 1
+	b |= b >> 2
+	b |= b >> 4
+	return b & ^(b >> 1)
+}
+
 func (t *RadixTree) Set(key string, val string) {
 	n := &t.root
-	var i uint32
+	var i int
 	for {
 		if *n == nil {
+			fmt.Println("Setting from nil")
 			*n = &tnode{key: key, val: val}
 			return
 		} else {
 			switch x := (*n).(type) {
-			case inode:
+			case *inode:
+				fmt.Printf("Going down inode %v\n", x)
 				i = x.critbyte
+				if i >= len(key) {
+					// what do
+				}
 				if key[i]&x.critmask == 0 {
 					n = &x.lc
 				} else {
 					n = &x.rc
 				}
-			case tnode:
-				// TODO: can maybe speed this comp up by starting from i
+			case *tnode:
+				fmt.Printf("Going down tnode %v\n", x)
 				equal := true
 				j := i
 				// TODO: figure out how to treat case where one string is
@@ -89,11 +107,16 @@ func (t *RadixTree) Set(key string, val string) {
 					x.val = val
 					return
 				} else {
-					// TODO: figure out differing bit, set vals in inode
-					// accordingly
-					in := inode{lc: nil, rc: nil, critbyte: 0, critmask: 0}
+					mask := msb_mask(x.key[j] ^ key[j])
+					in := inode{lc: nil, rc: nil, critbyte: j, critmask: mask}
 					*n = &in
-					n = &in.lc
+					if mask&key[j] == 0 {
+						in.rc = x
+						n = &in.lc
+					} else {
+						in.lc = x
+						n = &in.rc
+					}
 				}
 			}
 		}
@@ -106,10 +129,13 @@ func (t RadixTree) DumpContents() {
 
 func dumpContents(n node, indent int) {
 	switch x := n.(type) {
-	case inode:
+	case *inode:
 		dumpContents(x.lc, indent+2)
 		dumpContents(x.rc, indent+2)
-	case tnode:
-		fmt.Println("%s -> %s", x.key, x.val)
+	case *tnode:
+		for i := 0; i < indent; i++ {
+			fmt.Printf(" ")
+		}
+		fmt.Printf("%v -> %v\n", x.key, x.val)
 	}
 }
--- radix_tree_test.go
+++ radix_tree_test.go
@@ -1,13 +1,63 @@
 package radix_tree
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestRadixTree(t *testing.T) {
-	const nihongo = "日本語"
-	for i := 0; i < len(nihongo); i++ {
-		fmt.Printf("%v: %v\n", i, nihongo[i])
+func TestGetEmpty(t *testing.T) {
+	r := RadixTree{}
+	_, ok := r.Get("foo")
+	if ok == nil {
+		t.Error("Want ok != nil, got ok = nil")
 	}
+}
+
+func TestSetAndGetBasic(t *testing.T) {
+	r := RadixTree{}
+	r.Set("foo", "bar")
+	val, ok := r.Get("foo")
+	if ok != nil {
+		t.Errorf("Want ok == nil, got ok = %v", ok)
+	}
+	if val != "bar" {
+		t.Errorf("Want val == 'bar', got val = %v", val)
+	}
+}
+
+func TestGetUnsuccessful(t *testing.T) {
+	r := RadixTree{}
+	r.Set("fooey", "bara")
+	r.Set("fooing", "barb")
+	r.Set("foozle", "barc")
+	r.DumpContents()
+	if _, ok := r.Get("foo"); ok == nil {
+		t.Error("Want ok != nil, got ok == nil")
+	}
+	if _, ok := r.Get("fooe"); ok == nil {
+		t.Error("Want ok != nil, got ok == nil")
+	}
+	if _, ok := r.Get("fooeyz"); ok == nil {
+		t.Error("Want ok != nil, got ok == nil")
+	}
+}
+
+func TestSetAndGetCommonPrefix(t *testing.T) {
+	r := RadixTree{}
+	r.Set("fooey", "bara")
+	r.Set("fooing", "barb")
+	r.Set("foozle", "barc")
+	if _, ok := r.Get("foo"); ok == nil {
+		t.Error("Want ok != nil, got ok == nil")
+	}
+	val, ok := r.Get("fooey")
+	if ok != nil {
+		t.Errorf("Want ok == nil, got ok = %v", ok)
+	}
+	if val != "bara" {
+		t.Errorf("Want val == 'bara', got val = %v", val)
+	}
+}
+
+func TestSetAndGetSubstrings(t *testing.T) {
+
 }
